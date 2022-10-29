372e281528736138c0f6c004cf4f1915f7b2d3a5
Jose Luis Bautista <joseluis27@ciencias.unam.mx>
2022-10-29 11:13:52 -0500

    Metodo que crea una secuencia ciclica

--- src/classes/GeneradorSecuencia.py
+++ src/classes/GeneradorSecuencia.py
@@ -1,21 +1,7 @@
 from mimetypes import init
 import random
 
-class GenereadorSeceuencia:
-
-    def __init__(self, tamanoSecuencia) :
-        self.tamanoSecuencia = tamanoSecuencia
-
-    def creaSecuencia (self, tamanoSecuencia):
-        secuencia = []
-        while (tamanoSecuencia != len(secuencia)):
-            numeroGenerado = random.randint(1,100)
-            if(numeroGenerado  not in  secuencia):
-                secuencia.append(numeroGenerado)
-        return secuencia       
-
 def merge(arr, l, m, r):
-
     n1 = m - l + 1
     n2 = r - m
 
@@ -24,7 +10,6 @@ def merge(arr, l, m, r):
 
     for i in range(0, n1):
         L[i] = arr[l + i]
-
     for j in range(0, n2):
         R[j] = arr[m + 1 + j]
 
@@ -40,12 +25,10 @@ def merge(arr, l, m, r):
             arr[k] = R[j]
             j += 1
         k += 1
-
     while i < n1:
         arr[k] = L[i]
         i += 1
         k += 1
-
     while j < n2:
         arr[k] = R[j]
         j += 1
@@ -59,13 +42,45 @@ def mergeSort(arr, l, r):
         mergeSort(arr, l, m)
         mergeSort(arr, m+1, r)
         merge(arr, l, m, r)
+
+class GenereadorSeceuencia:
+
+    def __init__(self, tamanoSecuencia) :
+        self.tamanoSecuencia = tamanoSecuencia
+        self.secuencia = []
+        self.creaSecuencia(tamanoSecuencia)
+        self.creaSecuenciaCiclica()
+
+    def getSecuencia(self):
+        return self.secuencia
+
+    def creaSecuencia (self, tamanoSecuencia):
+        secuencia = []
+        while (tamanoSecuencia != len(secuencia)):
+            numeroGenerado = random.randint(1,100)
+            if(numeroGenerado  not in  secuencia):
+                secuencia.append(numeroGenerado)
+        mergeSort(secuencia,0,len(secuencia)-1)
+        self.secuencia = secuencia    
+
+    def creaSecuenciaCiclica(self):
+        indiceCiclo = random.randint(0, len(self.secuencia)-1)
+        i = 0
+        while (i < indiceCiclo):
+            j = 0
+            auxiliar = []
+            auxiliar.append(self.secuencia[j])
+            while(j < len(self.secuencia)):
+                if (j == len(self.secuencia)-1):
+                    self.secuencia[0] = auxiliar.pop(0)    
+                else:    
+                    auxiliar.append(self.secuencia[j+1])
+                    self.secuencia[j+1] = auxiliar.pop(0)
+                j += 1
+            i += 1        
     
 def main():
-    x = GenereadorSeceuencia(7)
-    s = x.creaSecuencia(7)
-    print(s)
-    mergeSort(s,0,len(s)-1)
-    print(s)
-    
-
+    """ x = GenereadorSeceuencia(10)
+    y = x.getSecuencia()
+    print(y) """
 main()
